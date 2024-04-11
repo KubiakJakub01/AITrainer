d537661ecc65ffa2256769f5ec4d098026b40d0e
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-11 22:52:37 +0200

    Fix local and global leader only wrapper functions

--- aitrainer/ddp.py
+++ aitrainer/ddp.py
@@ -1,21 +1,43 @@
 import os
+from collections.abc import Callable
+from functools import wraps
 
 
 def get_world_size():
     return int(os.environ.get('WORLD_SIZE', 1))
 
 
-def get_local_rank():
+def local_rank():
     return int(os.environ.get('LOCAL_RANK', 0))
 
 
-def get_global_rank():
+def global_rank():
     return int(os.environ.get('RANK', 0))
 
 
 def is_local_leader():
-    return get_local_rank() == 0
+    return local_rank() == 0
 
 
 def is_global_leader():
-    return get_global_rank() == 0
+    return global_rank() == 0
+
+
+def local_leader_only(fn) -> Callable:
+    @wraps(fn)
+    def wrapped(*args, **kwargs):
+        if is_local_leader():
+            return fn(*args, **kwargs)
+        return None
+
+    return wrapped
+
+
+def global_leader_only(fn) -> Callable:
+    @wraps(fn)
+    def wrapped(*args, **kwargs):
+        if is_global_leader():
+            return fn(*args, **kwargs)
+        return None
+
+    return wrapped

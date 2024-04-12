f672f7eb72e268fbac10bf118928fc7070d67f53
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-12 21:49:34 +0200

    Create train step function protocol

---
+++ aitrainer/protocols.py
@@ -0,0 +1,14 @@
+from typing import Any, Protocol
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from .hparams import Hparams
+
+
+class TrainStepFnProtocol(Protocol):
+    def __call__(
+        self, model: nn.Module, dl: DataLoader, device: torch.device, hparams: Hparams, step: int
+    ) -> tuple[Any, Any]:
+        ...

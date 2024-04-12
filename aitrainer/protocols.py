9f756ddb199fc22fe4df8a3e1a8d49303ea20a26
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-12 21:51:15 +0200

    Create valid step function protocol

--- aitrainer/protocols.py
+++ aitrainer/protocols.py
@@ -10,5 +10,12 @@ from .hparams import Hparams
 class TrainStepFnProtocol(Protocol):
     def __call__(
         self, model: nn.Module, dl: DataLoader, device: torch.device, hparams: Hparams, step: int
-    ) -> tuple[Any, Any]:
+    ) -> dict[str, Any]:
+        ...
+
+
+class ValidFnProtocol(Protocol):
+    def __call__(
+        self, model: nn.Module, dl: DataLoader, device: torch.device, hparams: Hparams, step: int
+    ) -> dict[str, Any]:
         ...

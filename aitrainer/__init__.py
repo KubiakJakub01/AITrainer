f70ac0ef9f4145db86dcaef911f292debfb70a56
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-19 22:08:09 +0200

    Add log functions to module init

--- aitrainer/__init__.py
+++ aitrainer/__init__.py
@@ -1,4 +1,5 @@
 from .hparams import Hparams
 from .pytorch_trainer import Trainer
+from .utils import log_debug, log_error, log_info, log_warning
 
-__all__ = ['Hparams', 'Trainer']
+__all__ = ['Hparams', 'Trainer', 'log_debug', 'log_info', 'log_warning', 'log_error']

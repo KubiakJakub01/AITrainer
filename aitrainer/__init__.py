52b6ed373e7078d646d4edb2542352ee867ba65d
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-19 22:09:43 +0200

    Add to_device function to module init

--- aitrainer/__init__.py
+++ aitrainer/__init__.py
@@ -1,5 +1,5 @@
 from .hparams import Hparams
 from .pytorch_trainer import Trainer
-from .utils import log_debug, log_error, log_info, log_warning
+from .utils import log_debug, log_error, log_info, log_warning, to_device
 
-__all__ = ['Hparams', 'Trainer', 'log_debug', 'log_info', 'log_warning', 'log_error']
+__all__ = ['Hparams', 'Trainer', 'log_debug', 'log_info', 'log_warning', 'log_error', 'to_device']

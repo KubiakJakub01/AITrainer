768e9a1a63580774b86130b05086e483d7e8103f
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-19 22:23:05 +0200

    Add params with deepspeed_config

--- aitrainer/hparams.py
+++ aitrainer/hparams.py
@@ -56,6 +56,44 @@ class Hparams:
         metadata={'help': 'Gradient clipping value'},
     )
 
+    # Optimizer
+    optimizer: str = field(
+        default='adam',
+        metadata={'help': 'Optimizer to use'},
+    )
+    lr: float = field(
+        default=1e-3,
+        metadata={'help': 'Learning rate'},
+    )
+
+    # Scheduler
+    warmup_min_lr: float = field(
+        default=0.0,
+        metadata={'help': 'Warmup minimum learning rate'},
+    )
+    warmup_max_lr: float = field(
+        default=1e-3,
+        metadata={'help': 'Warmup maximum learning rate'},
+    )
+    warmup_num_steps: int = field(
+        default=1000,
+        metadata={'help': 'Number of steps to warm up for'},
+    )
+
+    # Floating point
+    use_fp16: bool = field(
+        default=False,
+        metadata={'help': 'Use FP16'},
+    )
+    use_bf16: bool = field(
+        default=False,
+        metadata={'help': 'Use BF16'},
+    )
+    use_amp: bool = field(
+        default=False,
+        metadata={'help': 'Use AMP'},
+    )
+
     def __post_init__(self):
         self.train_data_dir = Path(self.train_data_dir)
         self.valid_data_dir = Path(self.valid_data_dir)
@@ -66,5 +104,32 @@ class Hparams:
         self.checkpoint_dir.mkdir(parents=True, exist_ok=True)
 
     @property
-    def deepspeed_config(self):
-        return {}
+    def deepspeed_config(self) -> dict:
+        return {
+            'train_micro_batch_size_per_gpu': self.batch_size,
+            'gradient_accumulation_steps': self.gradient_accumulation_steps,
+            'optimizer': {
+                'type': self.optimizer,
+                'lr': self.lr,
+            },
+            'scheduler': {
+                'type': 'WarmupDecayLR',
+                'params': {
+                    'warmup_min_lr': self.warmup_min_lr,
+                    'warmup_max_lr': self.warmup_max_lr,
+                    'warmup_num_steps': self.warmup_num_steps,
+                    'total_num_steps': self.total_steps,
+                    'warmup_type': 'linear',
+                },
+            },
+            'gradient_clipping': self.gradient_clipping,
+            'fp16': {
+                'enabled': self.use_fp16,
+            },
+            'bf16': {
+                'enabled': self.use_bf16,
+            },
+            'amp': {
+                'enabled': self.use_amp,
+            },
+        }

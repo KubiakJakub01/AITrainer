6185f9361117736793a50a840753129c027d0781
KubiakJakub01 <kubiak.jakub01@gmail.com>
2024-04-20 21:50:48 +0200

    Load mnist as example data

--- examples/data.py
+++ examples/data.py
@@ -1,18 +1,11 @@
-import random
-
+"""Load MNIST data from torchvision.datasets.MNIST."""
 from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import Compose, Normalize, ToTensor
 
 
-def get_random_data() -> list[int]:
-    """Get a random list of integers"""
-    return random.sample(range(1000), 1000)
-
-
-def get_random_data_loader(batch_size: int) -> DataLoader:
-    """Get a random data loader"""
-    return DataLoader(
-        get_random_data(),
-        batch_size=batch_size,
-        num_workers=0,
-        shuffle=True,
-    )
+def get_mnist_data_loader(batch_size: int) -> DataLoader:
+    """Get MNIST data loader."""
+    transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+    mnist = MNIST(root='data', train=True, download=True, transform=transform)
+    return DataLoader(mnist, batch_size=batch_size, shuffle=True)
